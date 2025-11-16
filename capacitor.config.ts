38aa75026fd38890c7feb53aa7173f0c58e58437
Shiv Kumar <95009691+ombhs@users.noreply.github.com>
2025-11-16 06:08:20 +0000

    --

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shivoham.studytracker',
+  appName: 'Study Tracker',
+  webDir: 'dist'
+};
+
+export default config;
